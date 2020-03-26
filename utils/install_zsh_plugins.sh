5f9b495abeaeaf7ed923a754d6dce42f35c5c060
Kurt Lachmann <lachmannk@gmail.com>
2020-03-26 16:54:36 +0000

    Reorganized the install scripts
    * New directory 'utils' that holds utility script (like installers)
    * New install script for zsh plugins

---
+++ utils/install_zsh_plugins.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+git clone https://github.com/zsh-users/zsh-history-substring-search /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+
