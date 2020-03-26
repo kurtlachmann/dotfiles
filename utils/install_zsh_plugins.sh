9863f33a68219b9988deef175c65d49515ec077b
Kurt Lachmann <lachmannk@gmail.com>
2020-03-26 19:53:20 +0000

    Corrected path in zsh install script

--- utils/install_zsh_plugins.sh
+++ utils/install_zsh_plugins.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-git clone https://github.com/zsh-users/zsh-history-substring-search /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search /usr/share/zsh/plugins/zsh-history-substring-search
 
